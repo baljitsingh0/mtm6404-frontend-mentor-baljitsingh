4c0fccefebc22abb147297cebbe8650e74a44a9a
baljitsingh0 <157618376+baljitsingh0@users.noreply.github.com>
2025-06-16 18:58:55 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navList = document.querySelector(".desktop-nav");
-
-menuToggle.addEventListener("click", () => {
-  navList.classList.toggle("active");
-});
